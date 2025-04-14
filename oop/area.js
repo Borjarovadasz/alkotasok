f567cc872e5f10acff733106e2994c7d7d0e6461
Meszaros Nandor Farkas <farkas.nandor.meszaros@gmail.com>
2025-04-14 22:51:47 +0200

    oop refactor 1

--- oop/area.js
+++ oop/area.js
@@ -19,44 +19,56 @@ class Area { // Egy class Area neven
      * 
      */
     constructor(className) { // Egy constructor fuggveny ami egy classname parametert var
+        const container = this.#getcontainerdiv();
+        this.#div = document.createElement('div')
+        this.#div.className = className; // Beallitja a privat #div osztalynevet a kapott classname ertekere
+        container.appendChild(this.#div); // Hozzaadja a privat #div elemet a container elemhez
+        
+    }
+    #getcontainerdiv() { // Egy privat metodus ami valoszinuleg visszaadja a container divet
         let containerDivecske = document.querySelector('.containeroop');  // Megprobalja megkeresni az osszes olyan elemet aminek az osztalyneve 'containeroop'
         if(!containerDivecske) { // Ha nem talal ilyen elemet akkor lép be az if-be
             containerDivecske = document.createElement('div') // Letrehoz egy uj div elemet
             containerDivecske.className = 'containeroop' // Beallitja az osztalynevet 'containeroop'-ra
             document.body.appendChild(containerDivecske) // Hozzaadja a body elemhez az uj divet
         }
-        this.#div = document.createElement('div'); // Letrehoz egy masik div elemet
-        this.#div.className = className; // Beallitja az osztalynevet a kapott classname parameter alapjan
-        containerDivecske.appendChild(this.#div) // Hozzaadja az uj divet a 'containeroop' divhez
+        return containerDivecske; //Visszaadja a containerdivecsket
     }
+     
 }
 /**
  * Az Asztal osztaly az Area osztalybol oroklodik, es fejcella elemekkel rendelkezo tablat hoz letre
  */
 
 class Asztal extends Area { 
+    
     /**
      * Ez a konstruktor urlapot hoz letre mezokkel, es hozzaadja a szulo div elemhez.
      * @param {string} className  A parameter ami egy string tipusu erteket var className neven
      */
     constructor(className) {// Egy constructor fuggveny ami egy styleclass parametert var
         super(className); // Meghivja a szuloosztaly constructor fuggvenyet a styleclass parameterrel
+        const tableBody = this.#asztalletrehozas(); 
+    }
+    #asztalletrehozas() {
         const asztal = document.createElement('table') // Letrehoz egy table elemet es eltárolja az asztal valtozoban
         this.div.appendChild(asztal) // Hozzaadja az asztal elemet a div elemhez amely a jelenlegi osztalyhoz tartozik
         const head = document.createElement('thead') // Letrehoz egy thead elemet es eltárolja a head valtozoban
         asztal.appendChild(head) // Hozzaadja a thead elemet az asztal elemhez
         const headrow = document.createElement('tr') // Letrehoz egy tr elemet sor es eltárolja a headrow valtozoban
         head.appendChild(headrow) // Hozzaadja a tr elemet a thead elemhez
-
+    
         const fejcellak = ['Szerzo', 'Mufaj', 'Cim'] // Egy tomb amely tartalmazza a fejlecek szoveget
         for(const fejtext of fejcellak) { // Vegigiteral a fejcellak tomb osszes elemen
             const fejcella = document.createElement('th') // Letrehoz egy th elemet es eltárolja a fejcella valtozoban
             fejcella.innerText = fejtext // Beallitja a th elem szoveget a fejtext ertekere
             headrow.appendChild(fejcella) // Hozzaadja a th elemet a tr elemhez
-        }
-        const tableBody = document.createElement('tbody'); // Letrehozunk egy tbody elemet
+        } const tableBody = document.createElement('tbody'); // Letrehozunk egy tbody elemet
         asztal.appendChild(tableBody); // Hozzaadjuk a tablehez
+        return tableBody; //Visszareturolni a tablebodyt
+       
     }
+    
 }
 /**
  * Ez az osztaly az Area osztalybol oroklodik, amely egy urlapot hoz letre, tartalmazva input mezoket es egy  gombot.
@@ -68,23 +80,11 @@ class Form extends Area { // Egy class Form ami az Area osztalybol oroklodik
      * Ez a konstruktor letrehoz egy urlapot sok mezovel amikhez a szulo ertekeit hozzadja.
      * @param {string} className   A parameter ami egy string tipusu erteket var className neven
      */
-    constructor(className) { // Egy constructor fuggveny ami egy classname parametert var
+    constructor(className, felementlist) { // Egy constructor fuggveny ami egy classname parametert var
         super(className) // Meghivja az oszto osztaly constructorat
         const formocsoka = document.createElement('form')  // Letrehoz egy form elemet es eltarolja a formocsoka valtozoban
         this.div.appendChild(formocsoka) // Hozzaadja a form elemet a div elemhez
-        const felementlist = [{ // Definial egy tombot amely tartalmazza a form mezok adatait
-            fid: 'Writer', // Az elso mezo id-je
-            label: 'Szerzo', // Az elso mezo labelje
-        },
-        {
-            fid: 'Genre', // A masodik mezo id-je
-            label: 'Mufaj', // A masodik mezo labelje
-        },
-        {
-            fid: 'Title', // A harmadik mezo id-je
-            label: 'Cim', // A harmadik mezo labelje
-        }
-    ]
+        
 
     for(const felement of felementlist) { // Vegigiteral a felementlist tomb elemein
         const fieldecske = makeAdiv('field');  // Letrehoz egy field divet es eltarolja a fieldecske valtozoban
--- oop/main.js
+++ oop/main.js
@@ -1,4 +1,19 @@
 const seperatorka = document.createElement('hr') // Letrehoz egy <hr> elemet es eltarolja a seperatorka valtozoban
 document.body.appendChild(seperatorka) // Hozzaadja a seperatorka elemet a body elemhez
+
+const felementlist = [{ // Definial egy tombot amely tartalmazza a form mezok adatait
+    fid: 'Writer', // Az elso mezo id-je
+    label: 'Szerzo', // Az elso mezo labelje
+},
+{
+    fid: 'Genre', // A masodik mezo id-je
+    label: 'Mufaj', // A masodik mezo labelje
+},
+{
+    fid: 'Title', // A harmadik mezo id-je
+    label: 'Cim', // A harmadik mezo labelje
+}
+]
+
 const asztal = new Asztal('table'); // Letrehoz egy uj Area objektumot 'table' osztalynevvel es eltarolja az asztal valtozoban
-const form = new Form('form');// Letrehoz egy uj Area objektumot 'form' osztalynevvel es eltarolja a form valtozoban
+const form = new Form('form', felementlist);// Letrehoz egy uj Area objektumot 'form' osztalynevvel es eltarolja a form valtozoban
