57e416a66b05b011f7bd24120c5bbce29c8ab70d
Meszaros Nandor Farkas <farkas.nandor.meszaros@gmail.com>
2025-04-14 20:48:56 +0200

    form letrehozasa sima

--- main.js
+++ main.js
@@ -36,6 +36,42 @@ tableelement.appendChild(tbody) // Hozzaadjuk a tablehez
 
 const formdivecske = makeAdiv('form')  // Letrehozunk egy masik divet, aminek az osztalyneve 'form'
 
+const formocska = document.createElement('form')  // Letrehozunk egy form elemet
+formdivecske.appendChild(formocska) // Hozzaadja a formocska elemet a formdivecske elemhez
+
+const fieldelementek = [{ // Definial egy tombot amely mezok adatait tartalmazza
+    id: 'Writer', // Az elso mezo azonositoja
+    label: 'Szerzo', // Az elso mezo cimkeje
+},
+{
+    id: 'Genre', // A masodik mezo azonositoja
+    label: 'Mufaj', // A masodik mezo cimkeje
+}, 
+{
+    id: 'title', // A harmadik mezo azonositoja
+    label: 'cim', // A harmadik mezo cimkeje
+}] 
+
+for(const element of fieldelementek) { // Vegigiteral a fieldelementek tomb osszes elemen
+    const mezo = makeAdiv('field') // Letrehoz egy field osztalyu divet es eltárolja a mezo valtozoban
+    formocska.append(mezo) // Hozzaadja a mezo elemet a formocska elemhez
+    const label = document.createElement('label') // Letrehoz egy label elemet es eltárolja a label valtozoban
+    label.htmlFor =  element.id // Beallitja a label elemhez tartozo input azonositot az element.id ertekere
+     label.textContent = element.label; // Beallitja a label elemenek szoveget az element.label ertekere
+     mezo.appendChild(label) // Hozzaadja a label elemet a mezo elemhez
+     const input = document.createElement('input') // Letrehoz egy input elemet es eltárolja az input valtozoban
+     input.id = element.id  // Beallitja az input elem azonositot az element.id ertekere
+     mezo.appendChild(document.createElement('br')) // Hozzaad egy sortores elemet a mezo elemhez
+     mezo.appendChild(input) // Hozzaadja az input elemet a mezo elemhez
+}
+
+
+const formbutton = document.createElement('button')// Letrehoz egy button elemet es eltárolja a formbutton valtozoban
+formbutton.textContent = "Hozzáadás!"  // Beallitja a gomb szoveget Hozzaadas szovegre
+formocska.appendChild(formbutton) // Hozzaadja a formbutton elemet a formocska elemhez
+
+
 
 containerDivecske.appendChild(tabledivecske) // A 'form' divhez hozzaadjuk a 'table' divet.
 containerDivecske.appendChild(formdivecske) // A 'container' divhez hozzaadjuk a 'form' divet.
+
